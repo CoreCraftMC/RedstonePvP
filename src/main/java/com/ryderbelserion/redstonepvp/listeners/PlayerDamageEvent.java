b0f07b602491461419d48449cf4a0173e024cad0
Ryder Belserion <no-reply@ryderbelserion.com>
2024-07-14 01:15:21 -0400

    create blood particle effect

---
+++ src/main/java/com/ryderbelserion/redstonepvp/listeners/PlayerDamageEvent.java
@@ -0,0 +1,18 @@
+package com.ryderbelserion.redstonepvp.listeners;
+
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class PlayerDamageEvent implements Listener {
+
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerHit(EntityDamageByEntityEvent event) {
+        if (!(event.getDamager() instanceof Player player) || !(event.getEntity() instanceof Player target)) return;
+
+        player.spawnParticle(Particle.BLOCK, target.getLocation(), 5, Material.REDSTONE_BLOCK.createBlockData());
+    }
+}
