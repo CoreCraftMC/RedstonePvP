153c330c1dd03da61e390d38d03b06edc45ec273
Ryder Belserion <no-reply@ryderbelserion.com>
2024-07-14 01:14:18 -0400

    add gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.paperweight)
+    alias(libs.plugins.shadowJar)
+    alias(libs.plugins.runPaper)
+
+    `paper-plugin`
+}
+
+rootProject.version = "1.0.0"
+
+repositories {
+    maven("https://repo.fancyplugins.de/releases")
+}
+
+dependencies {
+    paperweight.paperDevBundle(libs.versions.paper)
+
+    //implementation(libs.triumph.cmds)
+
+    implementation(libs.vital.paper)
+
+    //compileOnly(libs.fancy.holograms)
+}
+
+tasks {
+    runServer {
+        jvmArgs("-Dnet.kyori.ansi.colorLevel=truecolor")
+
+        defaultCharacterEncoding = Charsets.UTF_8.name()
+
+        minecraftVersion(libs.versions.minecraft.get())
+    }
+
+    shadowJar {
+        archiveBaseName.set(rootProject.name)
+        archiveClassifier.set("")
+    }
+
+    processResources {
+        inputs.properties("name" to rootProject.name)
+        inputs.properties("version" to project.version)
+        inputs.properties("group" to project.group)
+        inputs.properties("apiVersion" to libs.versions.minecraft.get())
+        inputs.properties("description" to project.properties["description"])
+        inputs.properties("website" to project.properties["website"])
+
+        filesMatching("paper-plugin.yml") {
+            expand(inputs.properties)
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "RedstonePvP"
+
+pluginManagement {
+    repositories {
+        maven("https://repo.crazycrew.us/releases")
+
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("com.ryderbelserion.feather-settings") version "0.0.4"
+}
