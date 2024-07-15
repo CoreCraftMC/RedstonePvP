a3b36e33a442c6b477c1760d6539379a95fcdbc5
Ryder Belserion <no-reply@ryderbelserion.com>
2024-07-14 20:54:45 -0400

    add item frame listener

--- src/main/java/com/ryderbelserion/redstonepvp/RedstonePvP.java
+++ src/main/java/com/ryderbelserion/redstonepvp/RedstonePvP.java
@@ -4,6 +4,9 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.ryderbelserion.redstonepvp.command.BaseCommand;
 import com.ryderbelserion.redstonepvp.command.subs.CommandReload;
 import com.ryderbelserion.redstonepvp.config.ConfigManager;
+import com.ryderbelserion.redstonepvp.listeners.PlayerDamageEvent;
+import com.ryderbelserion.redstonepvp.listeners.modules.PlayerFrequencyListener;
+import com.ryderbelserion.redstonepvp.listeners.modules.items.ItemFrameListener;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -20,6 +23,7 @@ public class RedstonePvP extends JavaPlugin {
         // Load the configuration.
         ConfigManager.load(getDataFolder());
 
+        // Register commands.
         getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, event -> {
             LiteralArgumentBuilder<CommandSourceStack> root = new BaseCommand().registerPermission().literal().createBuilder();
 
@@ -29,6 +33,13 @@ public class RedstonePvP extends JavaPlugin {
 
             event.registrar().register(root.build(), "the base command for RedstonePvP");
         });
+
+        List.of(
+                new PlayerFrequencyListener(),
+                new PlayerDamageEvent(),
+
+                new ItemFrameListener()
+        ).forEach(clazz -> getServer().getPluginManager().registerEvents(clazz, this));
     }
 
     @Override
---
+++ src/main/java/com/ryderbelserion/redstonepvp/listeners/modules/items/ItemFrameListener.java
@@ -0,0 +1,50 @@
+package com.ryderbelserion.redstonepvp.listeners.modules.items;
+
+import org.bukkit.Color;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.Inventory;
+
+public class ItemFrameListener implements Listener {
+
+    @EventHandler
+    public void onItemFrameDamage(EntityDamageByEntityEvent event) {
+        if (!(event.getEntity() instanceof ItemFrame)) return;
+
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onItemFrameRotate(PlayerInteractEntityEvent event) {
+        if (!(event.getRightClicked() instanceof ItemFrame)) return;
+
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onItemFrameInteract(PlayerInteractAtEntityEvent event) {
+        if (!(event.getRightClicked() instanceof ItemFrame itemFrame)) return;
+
+        final Player player = event.getPlayer();
+        final Inventory inventory = player.getInventory();
+
+        if (inventory.firstEmpty() == -1) {
+            player.spawnParticle(Particle.DUST, itemFrame.getLocation(), 1, new Particle.DustOptions(Color.RED, 1));
+
+            player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1f, 1f);
+
+            return;
+        }
+
+        inventory.addItem(itemFrame.getItem());
+
+        player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 1f);
+    }
+}
