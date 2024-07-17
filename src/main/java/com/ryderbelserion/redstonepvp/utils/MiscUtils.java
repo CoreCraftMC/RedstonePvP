7593046dbe8869cd194d728baebba59be72732c2
Ryder Belserion <no-reply@ryderbelserion.com>
2024-07-17 01:39:17 -0400

    add method to convert a string into a location

--- src/main/java/com/ryderbelserion/redstonepvp/utils/MiscUtils.java
+++ src/main/java/com/ryderbelserion/redstonepvp/utils/MiscUtils.java
@@ -1,17 +1,27 @@
 package com.ryderbelserion.redstonepvp.utils;
 
+import com.ryderbelserion.redstonepvp.RedstonePvP;
 import org.bukkit.Location;
 import org.jetbrains.annotations.NotNull;
+import java.util.UUID;
 
 public class MiscUtils {
 
-    public static @NotNull String location(@NotNull final Location location, boolean getName) {
+    private static final RedstonePvP plugin = RedstonePvP.getPlugin();
+
+    public static @NotNull String location(@NotNull final Location location, final boolean getName) {
         String name = getName ? location.getWorld().getName() : String.valueOf(location.getWorld().getUID());
 
-        return name + "," + location.getBlockX() + "," + location.getBlockY() + "," + location.getBlockZ();
+        return name + "," + location.getX() + "," + location.getY() + "," + location.getX();
     }
 
     public static @NotNull String location(@NotNull final Location location) {
         return location(location, false);
+    }
+
+    public static Location location(@NotNull final String location) {
+        final String[] split = location.split(",");
+
+        return new Location(plugin.getServer().getWorld(UUID.fromString(split[0])), Double.parseDouble(split[1]), Double.parseDouble(split[2]), Double.parseDouble(split[3]));
     }
 }
