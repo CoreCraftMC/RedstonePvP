22746ff701bc2d3d7a6403a84292592fd6c2fbfc
Ryder Belserion <no-reply@ryderbelserion.com>
2024-07-17 01:41:45 -0400

    add javadocs to beacon methods

--- src/main/java/com/ryderbelserion/redstonepvp/managers/BeaconManager.java
+++ src/main/java/com/ryderbelserion/redstonepvp/managers/BeaconManager.java
@@ -1,13 +1,17 @@
 package com.ryderbelserion.redstonepvp.managers;
 
 import com.ryderbelserion.redstonepvp.RedstonePvP;
+import com.ryderbelserion.redstonepvp.api.objects.BeaconDrop;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -18,6 +22,13 @@ public class BeaconManager {
     private static final DataManager dataManager = plugin.getDataManager();
 
     private static final Map<UUID, BeaconDrop> beaconDrops = new HashMap<>();
+
+    /**
+     * Adds a location to the cache and the database.
+     *
+     * @param uuid the uuid to identify a location
+     * @param location the location to add
+     */
     public static void addLocation(final UUID uuid, final String location) {
         // add to cache to ensure better performance in future checks.
         beaconDrops.put(uuid, new BeaconDrop(uuid, location));
@@ -39,6 +50,11 @@ public class BeaconManager {
         });
     }
 
+    /**
+     * Removes a location from the cache and the database.
+     *
+     * @param location the location to remove
+     */
     public static void removeLocation(final String location) {
         UUID uuid = null;
 
@@ -74,7 +90,12 @@ public class BeaconManager {
         });
     }
 
-    public static List<UUID> getLocations() {
+    /**
+     * Gets the active locations from either the cache or the database.
+     *
+     * @param queryDirectly true or false, decides whether to query the database or use the cache
+     * @return a list of uuids
+     */
     public static List<UUID> getLocations(final boolean queryDirectly) {
         if (!queryDirectly) {
             return beaconDrops.keySet().stream().toList();
@@ -102,7 +123,13 @@ public class BeaconManager {
         }).join();
     }
 
-    public static boolean hasLocation(final String location) {
+    /**
+     * Checks if a location already exists.
+     *
+     * @param location the location to check
+     * @param queryDirectly true or false, decides whether to query the database or use the cache
+     * @return true or false
+     */
     public static boolean hasLocation(final String location, final boolean queryDirectly) {
         if (!queryDirectly) {
             UUID uuid = null;
