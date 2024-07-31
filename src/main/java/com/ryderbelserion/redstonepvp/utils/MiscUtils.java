60838b3fa8b3fef925c7b8d4688b7fa2ff9d9f1d
Ryder Belserion <no-reply@ryderbelserion.com>
2024-07-31 14:43:34 -0400

    remove unneeded methods

--- src/main/java/com/ryderbelserion/redstonepvp/utils/MiscUtils.java
+++ src/main/java/com/ryderbelserion/redstonepvp/utils/MiscUtils.java
@@ -1,15 +1,7 @@
 package com.ryderbelserion.redstonepvp.utils;
 
 import com.ryderbelserion.redstonepvp.RedstonePvP;
-import com.ryderbelserion.redstonepvp.api.core.builders.InventoryBuilder;
-import com.ryderbelserion.redstonepvp.api.core.builders.types.settings.beacon.BeaconMenu;
-import com.ryderbelserion.redstonepvp.api.core.builders.types.settings.PlayersMenu;
-import com.ryderbelserion.redstonepvp.api.core.builders.types.settings.SettingsMenu;
-import com.ryderbelserion.redstonepvp.managers.ConfigManager;
-import com.ryderbelserion.redstonepvp.managers.config.Config;
-import com.ryderbelserion.redstonepvp.managers.config.beans.GuiProperty;
 import org.bukkit.Location;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import java.util.UUID;
 
@@ -31,19 +23,5 @@ public class MiscUtils {
         final String[] split = location.split(",");
 
         return new Location(plugin.getServer().getWorld(UUID.fromString(split[0])), Double.parseDouble(split[1]), Double.parseDouble(split[2]), Double.parseDouble(split[3]));
-    }
-
-    public static InventoryBuilder buildBeaconMenu(@NotNull final Player player) {
-        /*final GuiProperty gui = ConfigManager.getConfig().getProperty(Config.beacon_drop_menu);
-
-        return new BeaconMenu(player, gui.getTitle(), gui.getSize()).build();*/
-        return null;
-    }
-
-    public static InventoryBuilder buildPlayerMenu(@NotNull final Player player) {
-        /*final GuiProperty gui = ConfigManager.getConfig().getProperty(Config.players_menu);
-
-        return new PlayersMenu(player, gui.getTitle(), gui.getSize()).build();*/
-        return null;
     }
 }
