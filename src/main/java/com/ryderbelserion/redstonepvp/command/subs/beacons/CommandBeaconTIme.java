72cead032ccb522f33f527d8f466c73ba5d5e5da
Ryder Belserion <no-reply@ryderbelserion.com>
2024-08-03 02:01:30 -0400

    add time command

---
+++ src/main/java/com/ryderbelserion/redstonepvp/command/subs/beacons/CommandBeaconTIme.java
@@ -0,0 +1,55 @@
+package com.ryderbelserion.redstonepvp.command.subs.beacons;
+
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import com.ryderbelserion.redstonepvp.RedstonePvP;
+import com.ryderbelserion.vital.paper.commands.Command;
+import com.ryderbelserion.vital.paper.commands.CommandData;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
+import org.jetbrains.annotations.NotNull;
+import static io.papermc.paper.command.brigadier.Commands.argument;
+
+public class CommandBeaconTIme extends Command {
+
+    private final RedstonePvP plugin = RedstonePvP.getPlugin();
+
+    @Override
+    public void execute(final CommandData data) {
+
+    }
+
+    @Override
+    public @NotNull final String getPermission() {
+        return "redstonepvp.beacon.set";
+    }
+
+    @Override
+    public @NotNull final LiteralCommandNode<CommandSourceStack> literal() {
+        return Commands.literal("set")
+                .requires(source -> source.getSender().hasPermission(getPermission()))
+                .then(argument("name", StringArgumentType.string())
+                        .suggests((ctx, builder) -> suggestNames(builder))
+                .then(argument("time", IntegerArgumentType.integer())
+                        .suggests((ctx, builder) -> suggestIntegers(builder))
+                .executes(context -> {
+                   execute(new CommandData(context));
+
+                   return com.mojang.brigadier.Command.SINGLE_SUCCESS;
+                }))).build();
+    }
+
+    @Override
+    public @NotNull final Command registerPermission() {
+        final Permission permission = this.plugin.getServer().getPluginManager().getPermission(getPermission());
+
+        if (permission == null) {
+            this.plugin.getServer().getPluginManager().addPermission(new Permission(getPermission(), PermissionDefault.OP));
+        }
+
+        return this;
+    }
+}
