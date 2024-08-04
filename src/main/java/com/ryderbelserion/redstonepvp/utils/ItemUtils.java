e7e68416e0b8c4ccf9c2e33c8f1852526005b1f7
Ryder Belserion <no-reply@ryderbelserion.com>
2024-08-04 02:17:45 -0400

    update add buttons method

--- src/main/java/com/ryderbelserion/redstonepvp/utils/ItemUtils.java
+++ src/main/java/com/ryderbelserion/redstonepvp/utils/ItemUtils.java
@@ -14,7 +14,7 @@ public class ItemUtils {
 
     private static final Server server = plugin.getServer();
 
-    public static void addButtons(final GuiProperty property, final Player target, final PaginatedGui gui) {
+    public static void addButtons(final GuiProperty property, final PaginatedGui gui) {
         final ButtonProperty backButton = property.getBackButton();
         final ButtonProperty nextButton = property.getNextButton();
         final ButtonProperty menuButton = property.getMenuButton();
@@ -23,11 +23,13 @@ public class ItemUtils {
             gui.setItem(backButton.getDisplayRow(), backButton.getDisplayColumn(), gui.asGuiItem(new ItemBuilder().withType(backButton.getDisplayMaterial())
                     .setDisplayName(backButton.getDisplayName())
                     .setDisplayLore(backButton.getDisplayLore())
-                    .getStack(), back -> {
-                backButton.getSoundProperty().playSound(target);
+                    .getStack(), action -> {
+                if (!(action.getWhoClicked() instanceof Player clicker)) return;
 
-                backButton.getCommands().forEach(command -> server.dispatchCommand(server.getConsoleSender(), command.replaceAll("\\{player}", target.getName())));
-                backButton.getMessages().forEach(message -> MiscUtils.message(target, message));
+                backButton.getSoundProperty().playSound(clicker);
+
+                backButton.getCommands().forEach(command -> server.dispatchCommand(server.getConsoleSender(), command.replaceAll("\\{player}", clicker.getName())));
+                backButton.getMessages().forEach(message -> MiscUtils.message(clicker, message));
 
                 gui.previous();
             }));
@@ -37,11 +39,13 @@ public class ItemUtils {
             gui.setItem(nextButton.getDisplayRow(), nextButton.getDisplayColumn(), gui.asGuiItem(new ItemBuilder().withType(nextButton.getDisplayMaterial())
                     .setDisplayName(nextButton.getDisplayName())
                     .setDisplayLore(nextButton.getDisplayLore())
-                    .getStack(), next -> {
-                nextButton.getSoundProperty().playSound(target);
+                    .getStack(), action -> {
+                if (!(action.getWhoClicked() instanceof Player clicker)) return;
 
-                nextButton.getCommands().forEach(command -> server.dispatchCommand(server.getConsoleSender(), command.replaceAll("\\{player}", target.getName())));
-                nextButton.getMessages().forEach(message -> MiscUtils.message(target, message));
+                nextButton.getSoundProperty().playSound(clicker);
+
+                nextButton.getCommands().forEach(command -> server.dispatchCommand(server.getConsoleSender(), command.replaceAll("\\{player}", clicker.getName())));
+                nextButton.getMessages().forEach(message -> MiscUtils.message(clicker, message));
 
                 gui.next();
             }));
@@ -51,11 +55,13 @@ public class ItemUtils {
             gui.setItem(menuButton.getDisplayRow(), menuButton.getDisplayColumn(), gui.asGuiItem(new ItemBuilder().withType(menuButton.getDisplayMaterial())
                     .setDisplayName(menuButton.getDisplayName())
                     .setDisplayLore(menuButton.getDisplayLore())
-                    .getStack(), next -> {
-                menuButton.getSoundProperty().playSound(target);
+                    .getStack(), action -> {
+                if (!(action.getWhoClicked() instanceof Player clicker)) return;
 
-                menuButton.getCommands().forEach(command -> server.dispatchCommand(server.getConsoleSender(), command.replaceAll("\\{player}", target.getName())));
-                menuButton.getMessages().forEach(message -> MiscUtils.message(target, message));
+                menuButton.getSoundProperty().playSound(clicker);
+
+                menuButton.getCommands().forEach(command -> server.dispatchCommand(server.getConsoleSender(), command.replaceAll("\\{player}", clicker.getName())));
+                menuButton.getMessages().forEach(message -> MiscUtils.message(clicker, message));
             }));
         }
     }
