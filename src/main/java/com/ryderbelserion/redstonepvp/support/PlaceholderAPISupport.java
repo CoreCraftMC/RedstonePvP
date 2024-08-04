4a824d4a0bf08173990cddd15ba30560ad1218b0
Ryder Belserion <no-reply@ryderbelserion.com>
2024-08-04 00:49:01 -0400

    add pretty colors

--- src/main/java/com/ryderbelserion/redstonepvp/support/PlaceholderAPISupport.java
+++ src/main/java/com/ryderbelserion/redstonepvp/support/PlaceholderAPISupport.java
@@ -1,8 +1,11 @@
 package com.ryderbelserion.redstonepvp.support;
 
+import ch.jalu.configme.SettingsManager;
 import com.ryderbelserion.redstonepvp.RedstonePvP;
 import com.ryderbelserion.redstonepvp.api.objects.beacons.Beacon;
 import com.ryderbelserion.redstonepvp.managers.BeaconManager;
+import com.ryderbelserion.redstonepvp.managers.config.ConfigManager;
+import com.ryderbelserion.redstonepvp.managers.config.types.Config;
 import com.ryderbelserion.redstonepvp.utils.MiscUtils;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.OfflinePlayer;
@@ -13,20 +16,29 @@ public class PlaceholderAPISupport extends PlaceholderExpansion {
 
     private final RedstonePvP plugin = RedstonePvP.getPlugin();
 
+    private static final SettingsManager config = ConfigManager.getConfig();
+
     @Override
     public @NotNull final String onRequest(@Nullable final OfflinePlayer player, @NotNull final String identifier) {
         if (player == null || identifier.isEmpty()) return "N/A";
 
         String lower = identifier.toLowerCase();
 
+        final int requirement = config.getProperty(Config.beacon_drop_party_required_players);
+        final int onlinePlayers = this.plugin.getServer().getOnlinePlayers().size();
+
         for (final Beacon beacon : BeaconManager.getBeaconData().values()) {
             if (lower.equalsIgnoreCase(beacon.getName() + "_timer")) {
+                if (onlinePlayers == 0 || onlinePlayers < requirement) {
+                    return "<red>Not enough players</red>";
+                }
+
                 if (beacon.isActive()) {
-                    return "On Going";
+                    return "<green>On Going</green>";
                 }
 
                 if (beacon.isBroken()) {
-                    return "Out of Order";
+                    return "<red>Out of Order</red>";
                 }
 
                 return MiscUtils.convertTimeToString(beacon.getCalendar());
