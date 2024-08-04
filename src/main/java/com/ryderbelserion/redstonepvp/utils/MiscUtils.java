4ce673b46942ade8ee07ed87b218e2b467223d8a
Ryder Belserion <no-reply@ryderbelserion.com>
2024-08-03 23:46:12 -0400

    add methods for calendar calculations

--- src/main/java/com/ryderbelserion/redstonepvp/utils/MiscUtils.java
+++ src/main/java/com/ryderbelserion/redstonepvp/utils/MiscUtils.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+import java.util.Calendar;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
@@ -71,5 +72,51 @@ public class MiscUtils {
             item.setCanPlayerPickup(true);
             item.setCanMobPickup(false);
         }
+    }
+
+    public static Calendar getTimeFromString(final String time) {
+        Calendar calendar = Calendar.getInstance();
+
+        for (String i : time.split(" ")) {
+            if (i.contains("D") || i.contains("d")) calendar.add(Calendar.DATE, Integer.parseInt(i.replace("D", "").replace("d", "")));
+
+            if (i.contains("H") || i.contains("h")) calendar.add(Calendar.HOUR, Integer.parseInt(i.replace("H", "").replace("h", "")));
+
+            if (i.contains("M") || i.contains("m")) calendar.add(Calendar.MINUTE, Integer.parseInt(i.replace("M", "").replace("m", "")));
+
+            if (i.contains("S") || i.contains("s")) calendar.add(Calendar.SECOND, Integer.parseInt(i.replace("S", "").replace("s", "")));
+        }
+
+        return calendar;
+    }
+
+    public static String convertTimeToString(final Calendar timeTill) {
+        final Calendar rightNow = Calendar.getInstance();
+
+        int total = ((int) (timeTill.getTimeInMillis() / 1000) - (int) (rightNow.getTimeInMillis() / 1000));
+        int day = 0;
+        int hour = 0;
+        int minute = 0;
+        int second = 0;
+
+        for (; total > 86400; total -= 86400, day++);
+        for (; total > 3600; total -= 3600, hour++);
+        for (; total >= 60; total -= 60, minute++);
+
+        second += total;
+        String message = "";
+
+        if (day > 0) message += day + "d" + ", ";
+        if (day > 0 || hour > 0) message += hour + "h" + ", ";
+        if (day > 0 || hour > 0 || minute > 0) message += minute + "m" + ", ";
+        if (day > 0 || hour > 0 || minute > 0 || second > 0) message += second + "s" + ", ";
+
+        if (message.length() < 2) {
+            message = "0" + "s";
+        } else {
+            message = message.substring(0, message.length() - 2);
+        }
+
+        return message;
     }
 }
